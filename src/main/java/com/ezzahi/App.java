be34ef11c01c3b9eca8e1a6b55ccba788c267011
Amine2000-TDI <zahiamin4@gmail.com>
2025-03-07 17:56:10 +0000

    Préparation de l'environnement : ajout des dépendances nécessaires, configuration de la base de données (fichier persistence.xml) et des logs

---
+++ src/main/java/com/ezzahi/App.java
@@ -0,0 +1,9 @@
+package com.ezzahi;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class App {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
